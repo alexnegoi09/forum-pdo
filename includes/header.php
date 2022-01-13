71803fc7c9bf798c334d70e6e62b41f89a9ec0fd
alexnegoi09 <alexnegoi09@gmail.com>
2022-01-13 20:29:23 +0200

    completed header styling

--- includes/header.php
+++ includes/header.php
@@ -6,13 +6,13 @@ session_start();
 
 if (isset($_SESSION['username']) && isset($_SESSION['groups'])) { ?>
 
-    <header class="main-header-user">
+    <header class="main-header-user main-header">
             <h1 class="main-title"><a href="/forum-pdo/index.php">My Forum</a></h1>
             <div class="links">
                 <?php echo '<div class="logged-in"><a href="/forum-pdo/pages/profile.php?user_id=' . $_SESSION['user_id'] . '" class="user-link">' . $_SESSION['username'] . '</a> (' . $_SESSION['groups'] . ')</div>'; ?>
                 <div class="link-container">
                     <?php if ($_SESSION['groups'] === 'Administrator') { ?>
-                        <a href="/forum-pdo/pages/cpanel.php" class="header-link">Administrator Control Panel</a>
+                        <a href="/forum-pdo/pages/cpanel.php" class="header-link">Admin Control Panel</a>
                     <?php } else if ($_SESSION['groups'] === 'Moderator') { ?>
                         <a href="/forum-pdo/pages/cpanel.php" class="header-link">Moderator Control Panel</a>
                     <?php } else { ?>
@@ -25,7 +25,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['groups'])) { ?>
 
     <?php } else { ?>
 
-    <header class="main-header-guest">
+    <header class="main-header-guest main-header">
         <h1 class="main-title"><a href="/forum-pdo/index.php">My Forum</a></h1>
         <div class="links">
             <div class="logged-in">You are currently browsing the forum as a guest!</div>
