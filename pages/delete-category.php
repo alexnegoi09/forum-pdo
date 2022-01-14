2a73665ba290ac17046913a137b0e22fb266b44a
alexnegoi09 <alexnegoi09@gmail.com>
2022-01-14 19:23:35 +0200

    styled delete-thread.php, delete-category.php

--- pages/delete-category.php
+++ pages/delete-category.php
@@ -6,30 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Forum</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/delete-category.css">
 </head>
 <body>
-    
-</body>
-</html>
+
 <?php
-require('../classes/Database.php');
-require('../includes/header.php');
-require('../includes/logout.php'); 
-require('../classes/Category.php');
+    require('../classes/Database.php');
+    require('../includes/header.php');
+    require('../includes/logout.php'); 
+    require('../classes/Category.php');
 
 
-if (isset($_SESSION['username'])) {
+    if (isset($_SESSION['username'])) {
 
-    if ($_SESSION['groups'] !== 'Administrator') {
-        header('Location: /forum-pdo/index.php');
+        if ($_SESSION['groups'] !== 'Administrator') {
+            header('Location: /forum-pdo/index.php');
 
-    } else {
+        } else {
 
-    $category = new Category(null, null, $db);
-    $category->categoryCheck();
-    $category->delete();
-    echo '<p>Category deleted! Click <a href="/forum-pdo/index.php">here</a> to go back.</p>';
-    }
-}
+        $category = new Category(null, null, $db);
+        $category->categoryCheck();
+        $category->delete();
+        echo '<p class="text-success">Category deleted! Click <a href="/forum-pdo/index.php">here</a> to go back.</p>';
+        }
+    }   
 
 ?>
+
+    <script src="../js/user-color.js"></script> 
+</body>
+</html>
--- pages/delete-thread.php
+++ pages/delete-thread.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Forum</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/delete-thread.css">
 </head>
 <body>
     
@@ -27,10 +31,12 @@ if (isset($_SESSION['username'])) {
     $thread->categoryCheck();
     $thread->threadCheck();
     $thread->delete();
-    echo '<p>Thread deleted! Click <a href="/forum-pdo/pages/categories.php">here</a> to go back.</p>';
+    echo '<p class="text-success">Thread deleted! Click <a href="/forum-pdo/pages/categories.php">here</a> to go back.</p>';
     }
 }
 
 ?>
+
+<script src="../js/user-color.js"></script> 
 </body>
 </html>
