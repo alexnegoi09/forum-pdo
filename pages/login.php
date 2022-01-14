dd4b91d2c1b5a95de4624f5222d99153cb6abc7a
alexnegoi09 <alexnegoi09@gmail.com>
2022-01-14 12:07:54 +0200

    completed styling login.php

--- pages/login.php
+++ pages/login.php
@@ -17,28 +17,32 @@ if (isset($_COOKIE['remember'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in - My Forum</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/login.css">
 </head>
 <body>
-    <h1><a href="/forum-pdo/index.php">My Forum</a></h1>
-
-    <h2>Log in</h2>
+    <h1 class="main-header"><a href="/forum-pdo/index.php">My Forum</a></h1>
 
     <form action="login.php" class="login-form" method="POST">
+        <h2 class="main-title">Log in</h2>
         <p>
-            <label for="username">Username:</label>
-            <input type="text" name="username" class="username-input">
+            <label for="username" class="username-label">Username:</label>
+            <input type="text" name="username" class="form-control">
         </p>
         <p>
-            <label for="password">Password:</label>
-            <input type="password" name="password">
+            <label for="password" class="password-label">Password:</label>
+            <input type="password" name="password" class="form-control">
         </p>
         <p>
-            <label for="checkbox">Stay logged in</label>
-            <input type="checkbox" name="checkbox">
+            <button type="submit" name="submit" class="btn btn-success">Log in</button>
         </p>
         <p>
-            <button type="submit" name="submit">Log In</button>
-            or <a href="signup.php">create a new account.</a>
+            <label for="checkbox" class="checkbox-label form-check-label">Stay logged in</label>
+            <input type="checkbox" name="checkbox" class="form-check-input">
+        </p>
+        <p>
+            <a href="signup.php" class="account-link">Create a new account</a>
         </p>
     </form>
 </body>
@@ -52,7 +56,7 @@ if (isset($_POST['submit'])) {
         $login = new Login($_POST['username'], $_POST['password'], $db);
         $login->validate();
     } else {
-        echo '<p>Please enter a username and password!</p>';
+        echo '<p class="error">Please enter a username and password!</p>';
     }
    
     $db = null;
