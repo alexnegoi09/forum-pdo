dc2dc47053ec252d96e9483616de884e187d3a17
alexnegoi09 <alexnegoi09@gmail.com>
2022-01-20 02:56:45 +0200

    made small modification to logout.php

--- includes/logout.php
+++ includes/logout.php
@@ -3,7 +3,7 @@
 if (isset($_GET['action'])) {
     if ($_GET['action'] === 'logout') {
         $_SESSION = [];
-        setcookie('remember', $rememberToken, time() - 1, '/');
+        setcookie('remember', '', time() - 1, '/');
         header('Location: /forum-pdo/index.php');
         exit();
     } 
